ddf02ba13d296fc37401612a48f3f8ced36944cb
PCristian00 <cristianp9300@gmail.com>
2022-04-15 16:50:52 +0200

    Input

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def mia_len(elem):
-    lun=0
+    lun = 0
     for el in elem:
-        lun+=1
-    print("Lunghezza "+elem+" = "+str(lun))
+        lun += 1
+    print("Lunghezza " + elem + " = " + str(lun))
 
-mia_len("dee")
+
+e = input("Inserire lista o stringa: ")
+mia_len(e)
